9748641f1b3f52224610a24a7db3bf1f981ce384
Alexander Cochrane <acochrane@almightycorp.com>
2017-08-24 16:38:46 -0300

    completed first draft portfolio

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function showMoreInfo(language) {
 }
 
 $(function() {
+
     $("a").click(function(e) {
         if (this.hash !== "") {
             event.preventDefault();
@@ -14,4 +15,33 @@ $(function() {
             });
         }
     });
+
+    $("#astar").click(function(){
+        $("#astar-text").show();
+        $("#kinshift-text").hide();
+        $("#robot-text").hide();
+        $("#warlizard-text").hide();
+    });
+
+    $("#kinshift").click(function(){
+        $("#astar-text").hide();
+        $("#kinshift-text").show();
+        $("#robot-text").hide();
+        $("#warlizard-text").hide();
+    });
+
+    $("#robot").click(function(){
+        $("#astar-text").hide();
+        $("#kinshift-text").hide();
+        $("#robot-text").show();
+        $("#warlizard-text").hide();
+    });
+
+    $("#warlizard").click(function(){
+        $("#astar-text").hide();
+        $("#kinshift-text").hide();
+        $("#robot-text").hide();
+        $("#warlizard-text").show();
+    });
+
 });
