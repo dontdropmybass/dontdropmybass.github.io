31cf5f5dd48ca30d5c20152e8ee1353363ba3085
Alexander Cochrane <acochrane@almightycorp.com>
2018-05-29 21:09:58 -0300

    finished bbbs site

--- bbbs-site/js/item-lookup.js
+++ bbbs-site/js/item-lookup.js
@@ -3,6 +3,42 @@ let postCodeDates = [];
 let urbanFSAAreas = [];
 let dropOffItems = [];
 let noItems = [];
+let dropOffLocations = [];
+let map;
+let email = 'halifaxclothingdonations@bigbrothersbigsisters.ca';
+
+function initMap() {
+    let halifax = new google.maps.LatLng(44.65,-63.60);
+    map = new google.maps.Map(document.getElementById('binMap'), {
+        zoom: 10,
+        center: halifax,
+        scale: 2,
+        gestureHandling: 'cooperative',
+    });
+    for (i = 0; i < dropOffLocations.length; i++) {
+        new google.maps.Marker({
+            map: map,
+            position: new google.maps.LatLng(dropOffLocations[i][0], dropOffLocations[i][1]),
+            title: dropOffLocations[i][2],
+        });
+    }
+}
+
+function checkFormInputs() {
+    $("#formError").fadeOut('slow');
+    if ($("#name").val()!==''&&
+        $("#address").val()!==''&&
+        $("#postalCode").val()!==''&&
+        $("#phone").val()!==''&&
+        $("#phoneType").val()!==''&&
+        $("#email").val()!=='') {
+        return true;
+    }
+    else {
+        $("#formError").fadeIn('slow');
+        return false;
+    }
+}
 
 function setGlobalVariables(data,variable) {
     switch (variable) {
@@ -27,13 +63,19 @@ function setGlobalVariables(data,variable) {
         case 'no':
             noItems = data.split(/\r\n|\n/);
             break;
+        case 'dol':
+            lines = data.split(/\r\n|\n/);
+            for (let i = 0; i < lines.length; i++) {
+                dropOffLocations.push(lines[i].split('/'));
+            }
+            break;
     }
 }
 
 function itemLookup() {
     $("#itemText").fadeOut('slow', function() {
         let searchItem = $("#itemToSearch").val().toLowerCase();
-        isAccepted = false;
+        let isAccepted = false;
         for (i = 0; i < acceptedItems.length; i++) {
             if (searchItem === acceptedItems[i]) {
                 isAccepted = 'pickup';
@@ -56,25 +98,36 @@ function itemLookup() {
         if (isAccepted) {
             switch (isAccepted) {
                 case 'pickup':
-                    $("#postCodeSubtitle").html("We accept donations of " + searchItem + "! Please schedule a pick up time and we can pick it up, or you can drop it off to any of our many donation locations.");
+                    /* TODO: uncomment once backend post code dates implemented
+                    $("#postCodeSubtitle").html("We accept donations of " + searchItem + "! Please provide your contact details and we will touch base to schedule a pick up time, or you can drop it off to one of our many donation locations.");
+                    $("#itemText").fadeOut('slow');
                     $("#itemSearchForm").fadeOut('slow', function() {
                         $("#postCodeSearchForm").fadeIn('slow');
+                    });
+                    */
+                    // TODO: remove after backend post code dates implemented
+                    $("#dropOffSubtitle").html("We accept donations of " + searchItem + "!");
+                    $("#itemText").fadeOut('slow');
+                    $("#itemSearchForm").fadeOut('slow', function() {
+                        $("#dropOff").fadeIn('slow');
                     });
                     break;
                 case 'dropoff':
                     $("#valueVillageSubtitle").html("If you want to donate " + searchItem + ", we ask that you drop it off at Value Village Bayer's Lake.");
+                    $("#itemText").fadeOut('slow');
                     $("#itemSearchForm").fadeOut('slow', function() {
                         $("#valueVillageLocation").fadeIn('slow');
                     });
                     break;
                 case 'no':
-                    $("#itemText").html("I am sorry, but we cannot accept donations of " + searchItem + " at this time.");
+                    $("#itemText").html("We're sorry, but we cannot accept donations of " + searchItem + " at this time.");
+                    $("#itemText").fadeIn('slow');
                     //TODO: maybe link recycling?
                     break;
             }
         }
         else {
-            $("#itemText").html("Sorry, I could not find any mention of " + searchItem + ", I do not believe we accept it!");
+            $("#itemText").html("Hmmm - we can't seem to find " + searchItem + " on out list. Try using a more general term for the item, the singular or plural form, double check your spelling, or give us a call at +1 902 454 4025.");
         }
         $("#itemText").fadeIn('slow');
     });
@@ -196,6 +249,13 @@ $(document).ready(function() {
         error: function(request, status, error) {},
     });
 
+    $.ajax({
+        type: "GET",
+        url: "csv/bin-locations.csv",
+        success: function(response) {setGlobalVariables(response, 'dol');},
+        error: function(request, status, error) {},
+    });
+
     /** Page functions */
 
     $("#showItems").click(function() {
@@ -214,10 +274,14 @@ $(document).ready(function() {
     });
 
     $("#backButton").click(function() {
+        $("#itemText").html("");
+        $("#itemToSearch").val("");
+        $("#itemText").fadeOut('slow');
         $("#itemSearchForm").fadeOut('slow');
         $("#postCodeSearchForm").fadeOut('slow');
         $("#valueVillageLocation").fadeOut('slow');
         $("#schedulePickup").fadeOut('slow');
+        $("#dropOff").fadeOut('slow');
         $("#backButton").fadeOut('slow', function() {
             $("#buttons").fadeIn('slow');
         });
@@ -240,4 +304,33 @@ $(document).ready(function() {
             postCodeLookup();
         }
     });
+
+    $("#contactForm").submit(function(event) {
+        console.log(checkFormInputs());
+        if (!checkFormInputs()) {
+            event.preventDefault();
+        }
+        else {
+            /*$("#contactForm").fadeOut('slow');
+            $("#contactFormSubmitted").fadeIn('slow');*/
+        }
+    });
+
+    let url = window.location.href;
+    let captured = /formSubmitted=([^&]+)/.exec(url)[1];
+    if (captured) {
+        $("#buttons").hide();
+        $("#backButton").show();
+        $("#dropOff").show();
+        $("#contactForm").hide();
+        $("#contactFormSubmitted").show();
+    }
+
+    /*$.ajax({
+        url: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAhlmvdIXLMJLeCvpHhs9Gp7XnfWxuV85U',
+        type: 'GET',
+        dataType: 'jsonp',
+        cache: false,
+        success: function(){initMap();},
+    });*/
 });
